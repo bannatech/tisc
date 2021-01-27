6b763af48a7856b904eac588eb4984b4559e8efd
Paul Longtine <paul@nanner.co>
2021-01-27 18:36:26 -0500

    Fix missing call to 'free'

--- tisc.c
+++ tisc.c
@@ -543,6 +543,7 @@ int main(int argc, char *argv[])
 			if (process(line_number, &address, w_buffer, label, opcodes, args) == 0)
 			{
 				printf("Process: Error on line #%i\n", line_number);
+				free(w_buffer);
 				goto CLOSEFILES;
 			}
 		}
@@ -550,6 +551,8 @@ int main(int argc, char *argv[])
 		// Output Logisim raw v2.0 format
 
 		output_file(outputf, w_buffer, full_size);
+
+		free(w_buffer);
 	}
 
 CLOSEFILES:
